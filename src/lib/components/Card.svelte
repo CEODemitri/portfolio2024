d89046c675af322f531f0a53fe80e7127978a5fb
Starter <starter@Starters-MacBook-Pro.local>
2024-05-15 11:57:39 -0500

    more flavor to card component.

--- src/lib/components/Card.svelte
+++ src/lib/components/Card.svelte
@@ -1,54 +1,58 @@
 <script>
-    export const title = "Acme Inc. Redesign";
+	export const title = 'Default Title';
 </script>
 
-<div class="rounded border shadow-sm w-sm">
-    <div class="flex flex-col space-y-1.5 p-4">
-      <h3 class="text-2xl font-semibold leading-none tracking-tight">
-        {title}
-      </h3>
-      <p class="text-sm text-muted-foreground">
-        Redesigning the user experience and interface for a leading enterprise software company.
-      </p>
-    </div>
-    <div class="p-6">
-      <div class="flex items-center justify-center h-full">
-        <button class="justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 text-primary underline-offset-4 hover:underline h-10 px-4 py-2 flex items-center gap-2">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="24"
-            height="24"
-            viewBox="0 0 24 24"
-            fill="none"
-            stroke="currentColor"
-            stroke-width="2"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            class="h-5 w-5"
-          >
-            <path d="M2 12s3-7 10-7 10 7 10 7-3 7-10 7-10-7-10-7Z"></path>
-            <circle cx="12" cy="12" r="3"></circle>
-          </svg>
-          View Live
-        </button>
-        <button class="justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 text-primary underline-offset-4 hover:underline h-10 px-4 py-2 flex items-center gap-2">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="24"
-            height="24"
-            viewBox="0 0 24 24"
-            fill="none"
-            stroke="currentColor"
-            stroke-width="2"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            class="h-5 w-5"
-          >
-            <polyline points="16 18 22 12 16 6"></polyline>
-            <polyline points="8 6 2 12 8 18"></polyline>
-          </svg>
-          View Code
-        </button>
-      </div>
-    </div>
-  </div>
+<div class="rounded shadow-md w-[400px] h-72 bg-[url('./project_test.jpeg')]">
+	<div class="flex flex-col space-y-1.5 p-4 backdrop-brightness-50">
+		<h3 class="text-2xl font-semibold leading-none tracking-tight">
+			{title}
+		</h3>
+		<p class="text-sm text-muted-foreground">
+			Redesigning the user experience and interface for a leading enterprise software company.
+		</p>
+	</div>
+	<div class="p-6">
+		<div class="flex items-center justify-center h-full">
+			<button
+				class="justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 text-primary underline-offset-4 hover:underline h-10 px-4 py-2 flex items-center gap-2"
+			>
+				<svg
+					xmlns="http://www.w3.org/2000/svg"
+					width="24"
+					height="24"
+					viewBox="0 0 24 24"
+					fill="none"
+					stroke="currentColor"
+					stroke-width="2"
+					stroke-linecap="round"
+					stroke-linejoin="round"
+					class="h-5 w-5"
+				>
+					<path d="M2 12s3-7 10-7 10 7 10 7-3 7-10 7-10-7-10-7Z"></path>
+					<circle cx="12" cy="12" r="3"></circle>
+				</svg>
+				View Live
+			</button>
+			<button
+				class="justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 text-primary underline-offset-4 hover:underline h-10 px-4 py-2 flex items-center gap-2"
+			>
+				<svg
+					xmlns="http://www.w3.org/2000/svg"
+					width="24"
+					height="24"
+					viewBox="0 0 24 24"
+					fill="none"
+					stroke="currentColor"
+					stroke-width="2"
+					stroke-linecap="round"
+					stroke-linejoin="round"
+					class="h-5 w-5"
+				>
+					<polyline points="16 18 22 12 16 6"></polyline>
+					<polyline points="8 6 2 12 8 18"></polyline>
+				</svg>
+				View Code
+			</button>
+		</div>
+	</div>
+</div>
