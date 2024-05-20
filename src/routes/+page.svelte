e64c422d75c2e1b9cd9f0c0f92d7d64ad6108b68
ceoDemitri <demichole@gmail.com>
2024-05-20 11:58:23 -0500

    took away gsap script for error to go away.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -52,35 +52,6 @@
 	function switchJob() {
 		jobSpace.innerHTML = jobs[Math.floor(Math.random() * jobs.length)];
 	}
-
-	import { gsap } from 'gsap';
-
-	import { CustomEase } from 'gsap/CustomEase';
-	import { RoughEase, ExpoScaleEase, SlowMo } from 'gsap/EasePack';
-
-	import { Flip } from 'gsap/Flip';
-	import { ScrollTrigger } from 'gsap/ScrollTrigger';
-	import { ScrollToPlugin } from 'gsap/ScrollToPlugin';
-	import { Draggable } from 'gsap/Draggable';
-	import { MotionPathPlugin } from 'gsap/MotionPathPlugin';
-	import { EaselPlugin } from 'gsap/EaselPlugin';
-	import { PixiPlugin } from 'gsap/PixiPlugin';
-	import { TextPlugin } from 'gsap/TextPlugin';
-
-	gsap.registerPlugin(
-		Flip,
-		ScrollTrigger,
-		ScrollToPlugin,
-		Draggable,
-		MotionPathPlugin,
-		EaselPlugin,
-		PixiPlugin,
-		TextPlugin,
-		RoughEase,
-		ExpoScaleEase,
-		SlowMo,
-		CustomEase
-	);
 </script>
 
 <div class="flex flex-col min-h-[100dvh]">
