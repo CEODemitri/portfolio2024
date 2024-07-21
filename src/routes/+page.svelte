2c87282ea8106b9065532249142871ef1182793d
ceoDemitri <demichole@gmail.com>
2024-07-21 17:48:37 -0500

    starting with gsap, added daisy

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,19 +1,26 @@
 <script>
+	import { onMount } from 'svelte';
 	import { gsap } from 'gsap';
 
-	gsap.to('#box', { rotation: 27, x: 100, duration: 1 });
+	onMount(() => {
+		try {
+			gsap.to('.box', { rotation: 27, x: 100, duration: 1 });
+		} catch (error) {
+			console.error('Error in GSAP animation:', error);
+		}
+	});
 </script>
 
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
-<div class="card w-96 bg-base-100 shadow-xl">
+<div class="card w-96 bg-base-100 shadow-xl box">
 	<figure>
 		<img
 			src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
 			alt="Shoes"
 		/>
 	</figure>
-	<div class="card-body" id="box">
+	<div class="card-body">
 		<h2 class="card-title">Shoes!</h2>
 		<p>If a dog chews shoes whose shoes does he choose?</p>
 		<div class="card-actions justify-end">
