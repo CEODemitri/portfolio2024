3602963c78e9add0c97e7698c0d239c44c6c662c
ceoDemitri <demichole@gmail.com>
2024-07-23 07:45:03 -0500

    my bg

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -55,13 +55,10 @@
 
 <div class="flex flex-col min-h-[100dvh]">
 	<main class="font-['Josefin_Sans_Variable']">
-		<div
-			class="hero min-h-[80vh]"
-			style="background-image: url(https://img.daisyui.com/images/stock/photo-1507358522600-9f71e620c44e.webp);"
-		>
+		<div class="hero min-h-[80vh]" style="background-image: url(/bg/lg.jpeg);">
 			<div class="hero-overlay bg-opacity-60"></div>
 			<div class="hero-content text-neutral-content text-center">
-				<div class="space-y-4 bg-slate-800 px-4 py-8 rounded-lg">
+				<div class="space-y-4 bg-slate-800/90 px-4 py-8 rounded-lg">
 					<h1
 						class="name text-4xl font-bold tracking-wide sm:text-5xl md:text-6xl font-['Rubik_Dirt']"
 					>
@@ -175,16 +172,14 @@
 				</div>
 			</div>
 		</section>
-		<section id="skills" class="w-full py-12 md:py-24 lg:py-32">
+		<section id="skills" class="w-full py-12 md:py-24 lg:py-32 bg-slate-800">
 			<div class="grid gap-6 px-4 md:px-6">
 				<div class="flex flex-col items-center justify-center space-y-4 text-center">
-					<div class="space-y-2">
+					<div class="space-y-2 text-neutral-300">
 						<h2 class="text-xl font-bold tracking-wide sm:text-3xl">
 							Here are some of the technologies I'm proficient in.
 						</h2>
-						<p
-							class="max-w-[900px] text-muted-foreground md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed"
-						>
+						<p class="max-w-[900px] md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed">
 							My Skills
 						</p>
 					</div>
